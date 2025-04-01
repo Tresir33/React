55c8f9d663b578661e9150c1c46b1560d7de9a81
Tresir33 <tresilaho33@gmail.com>
2025-04-01 20:52:00 +0200

    hide secret info

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBmLFlyfaqJK3nTw2aipm4yOfx22ojHIm0",
-    authDomain: "connectify-23aaa.firebaseapp.com",
-    projectId: "connectify-23aaa",
-    storageBucket: "connectify-23aaa.firebasestorage.app",
-    messagingSenderId: "11544995696",
-    appId: "1:11544995696:web:28597bc9143a4e67c511b5"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
